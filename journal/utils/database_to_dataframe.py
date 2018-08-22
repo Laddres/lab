e83e07f1479c9053d54a0c4afbc287453ff86d6f
ranpa <rangelpassos@hotmail.com>
2018-08-22 01:01:54 -0300

    feature: lib para facilitar o acesso pandas<->mysql

---
+++ journal/utils/database_to_dataframe.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import mysql.connector as mysql
+
+BD_CONFIG = {
+    'user': 'root',
+    'password': '',
+    'host': '127.0.0.1',
+    'database': 'laddres'
+}
+
+def database_to_dataframe(tabela, colunas_tabela, condicao):
+    cnx = mysql.connect(**BD_CONFIG)
+
+    columns = ', '.join(colunas_tabela) if colunas_tabela else '*'
+    df = pd.read_sql(
+        'SELECT {} FROM {} {}'.format(columns, tabela, condicao),
+        con=cnx
+    )
+
+    return df
